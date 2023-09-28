1afc5927b5136d583a328d26b5b9a6a610145a8d
Pietu R <pietu_r@gmx.com>
2023-09-28 16:00:37 +0300

    scaffold dev environment

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            pandoc
+            gnumake
+            gnused
+          ];
+        };
+      }
+    );
+}
